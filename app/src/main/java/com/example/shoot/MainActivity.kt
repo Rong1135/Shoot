a8d072ceb6f4da6f5583212fafba1b935e250451
Rong1135 <s1090343@gm.pu.edu.tw>
2022-05-11 21:51:28 +0800

    v.3

--- app/src/main/java/com/example/shoot/MainActivity.kt
+++ app/src/main/java/com/example/shoot/MainActivity.kt
@@ -5,6 +5,11 @@ import android.os.Bundle
 import android.widget.ImageView
 import kotlinx.coroutines.*
 import android.graphics.Canvas
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+@GlideModule
+public  final class MyAppGlideModule : AppGlideModule()
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,19 +24,17 @@ class MainActivity : AppCompatActivity() {
         img = findViewById(R.id.img)
         game = findViewById(R.id.game)
 
-        img.setOnClickListener({
-            if (flag)
-            {
+        img.setOnClickListener {
+            if (flag) {
                 flag = false
                 img.setImageResource(R.drawable.start)
                 job.cancel()
-            }
-            else
-            {
+            } else {
                 flag = true
                 img.setImageResource(R.drawable.stop)
-                job = GlobalScope.launch(Dispatchers.Main) {
-                    while(flag) {
+                job = GlobalScope.launch(Dispatchers.Main)
+                {
+                    while (flag) {
                         delay(10)
                         var canvas: Canvas = game.surfaceHolder.lockCanvas()
                         game.drawSomething(canvas)
@@ -39,7 +42,15 @@ class MainActivity : AppCompatActivity() {
                     }
                 }
             }
+        }
 
-        })
+        val img2:ImageView = findViewById(R.id.img2)
+        GlideApp.with(this)
+            .load(R.drawable.myphoto)
+            .circleCrop()
+            .override(100, 100)
+            .into(img2)
+
     }
 }
+
